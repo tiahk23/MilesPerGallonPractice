32ce6373a6746ccb8260d08453062b84eeadc9d0
Tiah Keefe <tetered12@gmail.com>
2021-07-12 20:35:43 -0500

    Add project files.

---
+++ MilesPerGallonPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MilesPerGallonPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

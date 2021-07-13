5d391adcbd867d882d5ae4d972b8d51405dd65b4
Tiah Keefe <tetered12@gmail.com>
2021-07-12 20:41:31 -0500

    Created code to calculate miles per gallon taking user input.

--- MilesPerGallonPractice/Program.cs
+++ MilesPerGallonPractice/Program.cs
@@ -6,7 +6,12 @@ namespace MilesPerGallonPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("How many miles have you driven?");
+            string miles = Console.ReadLine();
+            Console.WriteLine("How much gas have you used (in gallons)?");
+            string gas = Console.ReadLine();
+            int milesPerGallon = int.Parse(miles) / int.Parse(gas);
+            Console.WriteLine("Your miles per Gallon is " + milesPerGallon);
         }
     }
 }
